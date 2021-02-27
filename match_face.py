c07aea41e2f1adfb5c267737120a05d923319f7b
soundarzozm <soundar.murugan1@learner.manipal.edu>
2021-02-28 01:03:05 +0530

    add url support

--- match_face.py
+++ match_face.py
@@ -1,30 +1,35 @@
 import face_recognition as fr
-import sys
+import numpy as np
+from PIL import Image
+import io
+import requests
 
 known_path = sys.argv[1]
 unknown_path = sys.argv[2]
 
-def compare_faces(known_encoding, unknown_encoding):
+def compare_faces(known_encoding, unknown_encoding, tolerance):
 
-    result = fr.compare_faces([known_encoding], unknown_encoding, tolerance=0.7)
+    result = fr.compare_faces([known_encoding], unknown_encoding, tolerance=tolerance)
 
     return result
 
+def im2numpy(url):
+    
+    response = requests.get(url)
+    img = Image.open(io.BytesIO(response.content))
+    img.show()
+    numpydata = np.asarray(img)
+    return numpydata
 
 def detect_faces(known_path, unknown_path):
-
-    try:
         
-        known_image = fr.load_image_file(known_path)
-        unknown_image = fr.load_image_file(unknown_path)
-        
-        known_encoding = fr.face_encodings(known_image)[0]
-        unknown_encoding = fr.face_encodings(unknown_image)[0]
-    
-    except:
-        return "Please retry!"    
+    known_image = im2numpy(known_path)
+    unknown_image = im2numpy(unknown_path)
+    known_encoding = fr.face_encodings(known_image)[0]
+    unknown_encoding = fr.face_encodings(unknown_image)[0]
+   
 
-    result = compare_faces(known_encoding, unknown_encoding)
+    result = compare_faces(known_encoding, unknown_encoding, 0.75)
 
     return result[0]
 
